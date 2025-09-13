af81db8345572e3fb1e3525f0a7b9281fc4dc4d3
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-09-13 10:48:43 +0530

    Connection to database

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+import { DB_NAME } from "../constants.js";
+
+async function connectDB(){
+  try {
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    return connection;
+  } catch (error) {
+    throw error;
+  }
+} 
+
+export default connectDB;

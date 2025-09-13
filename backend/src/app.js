6b43f82063137a6b7cd6ff39c93c1f57e08bccd5
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-09-13 19:53:32 +0530

    Add cookie-parser dependency and implement error handling middleware
    - Added cookie-parser to package.json and package-lock.json.
    - Created errorHandler middleware for standardized error responses.
    - Implemented ApiResponse class for structured API responses.
    - Refactored app.js to use cookie-parser and organized imports.
    - Added asyncHandler utility for handling asynchronous route handlers.

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors({origin: process.env.CORS_ORIGIN}));
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+
+export default app;
---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,6 @@
+export default class ApiError {
+    constructor(message, statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    } 
+}   
